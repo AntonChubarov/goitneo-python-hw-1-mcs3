352d4519a3778dff96f8ab2a74e34641ae06aef5
AntonChubarov <achlp600@gmail.com>
2023-10-06 16:51:10 +0200

    feat: implemented basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Colleagues birthday assistant
-Work in progress
+From goitneo-python-hw-1-mcs3 folder run:
+```console
+python3 main.py --file ./data/users.csv
+```
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import argparse
 import csv
+import datetime
 
 
 def load_users_from_file(path):
@@ -7,18 +9,49 @@ def load_users_from_file(path):
         data = []
     
         for row in csv_reader:
+            row['birthday'] = datetime.datetime.strptime(row['birthday'], "%Y-%m-%d")
+            
             data.append(row)
 
     return data
 
 
 def get_birthdays_per_week(users):
-    pass
+    today = datetime.datetime.today().date()
+
+    users_to_congratulate = {}
+
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()
+        birthday_this_year = birthday.replace(year=today.year)
+
+        if birthday_this_year < today:
+            birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+
+        delta_days = (birthday_this_year - today).days
+
+        if delta_days < 7:
+            day_name = birthday_this_year.strftime("%A")
+
+            if day_name in ('Saturday', 'Sunday'):
+                day_name = 'Monday'
+
+            if day_name not in users_to_congratulate:
+                users_to_congratulate[day_name] = []
+
+            users_to_congratulate[day_name].append(name)
+
+    for day, names in users_to_congratulate.items():
+        print('{day}: {names}'.format(day=day, names=', '.join(names)))
 
 
 if __name__ == '__main__':
-    users = load_users_from_file('./data/users.csv')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, help='Path to the csv file with users data', default='./data/users.csv')
+    
+    args = parser.parse_args()
 
-    print(users)
+    users = load_users_from_file(args.file)
 
     get_birthdays_per_week(users)
